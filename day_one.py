07f8b36bcc8ad0aa9623061699ddd1b1825d99e7
Neil Jeffery <neil.jeffery@bt.com>
2022-11-29 13:18:07 +0000

    Day One Skeleton Setup

---
+++ day_one.py
@@ -0,0 +1,9 @@
+""" Solution to Day 1 Advent of Code 2022"""
+
+
+def main():  # pylint:disable=missing-function-docstring
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_day_one.py
@@ -0,0 +1,2 @@
+""" Tests for Solution to Day 1 Advent of Code 2022"""
+import day_one
