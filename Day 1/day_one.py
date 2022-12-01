9d66fa0b449a2c10dc374af57607f40a703aa09a
Neil Jeffery <neil.jeffery@bt.com>
2022-12-01 11:54:09 +0000

    Added better typehints

--- Day 1/day_one.py
+++ Day 1/day_one.py
@@ -1,23 +1,25 @@
 """ Solution to Day 1 Advent of Code 2022"""
+from typing import List
+
 from day_one_input import puzzle_input
 
 
-def process_input(multiline_input: str) -> list:
+def process_input(multiline_input: str) -> List[List[str]]:
     """Process Multi-line input string to list of lists"""
     return [sub_list.splitlines() for sub_list in multiline_input.split("\n\n")]
 
 
-def summarise_entries(lists: list) -> list:
+def summarise_entries(lists: List[List[str]]) -> List[int]:
     """Takes a 2D array and computes the sum on each sublist"""
     return [sum_list(entry) for entry in lists]
 
 
-def sum_list(my_list: list) -> int:
+def sum_list(my_list: List[str]) -> int:
     """Returns integer sum of values in list"""
     return sum(int(entry) for entry in my_list)
 
 
-def sum_highest_values(my_list: list, number_of_values: int = 1) -> int:
+def sum_highest_values(my_list: List[int], number_of_values: int = 1) -> int:
     """
     Returns sum of highest values in a list
     number_of_values is number of values to consider
