83c8c16aeb6e0e807ec535fd143c59b53bc7c1d4
Neil <31366098+neilneil2000@users.noreply.github.com>
2022-12-04 10:02:44 +0000

    Move elf cleaning assignment class to own file

--- Day 4/day_four.py
+++ Day 4/day_four.py
@@ -1,34 +1,9 @@
 """Solution to Day 4 of Advent of Code 2022"""
-from __future__ import annotations
 
 from typing import List
 
 from day_four_input import PUZZLE_INPUT
-
-
-class ElfCleaningAssignment:
-    """Representation of Cleaning Assignment given to an elf"""
-
-    def __init__(self, sectors: str):
-        self.start = 0
-        self.end = 0
-        self.__process_definition(sectors)
-
-    def __process_definition(self, sectors: str):
-        """Process 'X-Y' cleaning definition into sectors"""
-        self.start, self.end = map(int, sectors.split("-"))
-
-    def is_superset(self, other: ElfCleaningAssignment):
-        """Returns True if self is superset of other"""
-        return self.start <= other.start and self.end >= other.end
-
-    def is_superset_or_subset(self, other: ElfCleaningAssignment):
-        """Returns True if either class is superset of the other"""
-        return self.is_superset(other) or other.is_superset(self)
-
-    def is_overlap(self, other: ElfCleaningAssignment):
-        """Returns True if there is any overlap between objects"""
-        return not (self.start > other.end or self.end < other.start)
+from elf_cleaning_assignment import ElfCleaningAssignment
 
 
 def process_input() -> List[List[str]]:
@@ -44,12 +19,12 @@ def build_elf_list() -> List[List[ElfCleaningAssignment]]:
     ]
 
 
-def number_of_subsets(elf_pairs):
+def number_of_subsets(elf_pairs: List[ElfCleaningAssignment]):
     """Return number of pairs with a complete overlap (i.e. one is a subset of the other)"""
     return sum(elf_one.is_superset_or_subset(elf_two) for elf_one, elf_two in elf_pairs)
 
 
-def number_of_overlaps(elf_pairs):
+def number_of_overlaps(elf_pairs:List[ElfCleaningAssignment]):
     """Return number of pairs with any overlap"""
     return sum(elf_one.is_overlap(elf_two) for elf_one, elf_two in elf_pairs)
 
---
+++ Day 4/elf_cleaning_assignment.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+
+class ElfCleaningAssignment:
+    """Representation of Cleaning Assignment given to an elf"""
+
+    def __init__(self, sectors: str):
+        self.start = 0
+        self.end = 0
+        self.__process_definition(sectors)
+
+    def __process_definition(self, sectors: str):
+        """Process 'X-Y' cleaning definition into sectors"""
+        self.start, self.end = map(int, sectors.split("-"))
+
+    def is_superset(self, other: ElfCleaningAssignment):
+        """Returns True if self is superset of other"""
+        return self.start <= other.start and self.end >= other.end
+
+    def is_superset_or_subset(self, other: ElfCleaningAssignment):
+        """Returns True if either class is superset of the other"""
+        return self.is_superset(other) or other.is_superset(self)
+
+    def is_overlap(self, other: ElfCleaningAssignment):
+        """Returns True if there is any overlap between objects"""
+        return not (self.start > other.end or self.end < other.start)
