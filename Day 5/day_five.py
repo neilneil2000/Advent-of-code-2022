9ef11746d87d102a9e2e1d5b6a98140bfe678fc6
Neil Jeffery <neil.jeffery@bt.com>
2022-12-05 11:52:58 +0000

    Move Input Processor to separate file

--- Day 5/day_five.py
+++ Day 5/day_five.py
@@ -1,32 +1,8 @@
 """Advent of Code 2022 Day 5"""
-from string import ascii_uppercase
 
 from crane_mover import CraneMover9000, CraneMover9001
 from day_five_input import CRATES, INSTRUCTIONS
-
-
-class InputProcessor:
-    """Class to Process input from file"""
-
-    @classmethod
-    def get_crates(cls, raw_input: str):
-        """Return formatted list of crates from input format"""
-        crates = cls.__initialise_crate_stack(cls.__number_of_crate_stacks(raw_input))
-        crate_stack = raw_input.splitlines()[:-1]
-        for line in crate_stack:
-            for index, character in enumerate(line):
-                if character in ascii_uppercase:
-                    crates[(index - 1) // 4].append(character)
-
-        return [stack[::-1] for stack in crates]
-
-    @staticmethod
-    def __initialise_crate_stack(number_of_stacks: int) -> list:
-        return [[] for _ in range(number_of_stacks)]
-
-    @staticmethod
-    def __number_of_crate_stacks(raw_input: str) -> int:
-        return int(raw_input.splitlines()[-1].split()[-1])
+from input_processor import InputProcessor
 
 
 def main():  # pylint:disable=missing-function-docstring
---
+++ Day 5/input_processor.py
@@ -0,0 +1,26 @@
+"""Input Processor for Advent of Code 2022 Day 5"""
+from string import ascii_uppercase
+
+
+class InputProcessor:
+    """Class to Process input from file"""
+
+    @classmethod
+    def get_crates(cls, raw_input: str):
+        """Return formatted list of crates from input format"""
+        crates = cls.__initialise_crate_stack(cls.__number_of_crate_stacks(raw_input))
+        crate_stack = raw_input.splitlines()[:-1]
+        for line in crate_stack:
+            for index, character in enumerate(line):
+                if character in ascii_uppercase:
+                    crates[(index - 1) // 4].append(character)
+
+        return [stack[::-1] for stack in crates]
+
+    @staticmethod
+    def __initialise_crate_stack(number_of_stacks: int) -> list:
+        return [[] for _ in range(number_of_stacks)]
+
+    @staticmethod
+    def __number_of_crate_stacks(raw_input: str) -> int:
+        return int(raw_input.splitlines()[-1].split()[-1])
