4f7c3c941a2b2ac457ab128e8a88fa57d8419698
Neil Jeffery <neil.jeffery@bt.com>
2022-12-05 11:50:12 +0000

    Create InputProcessor

--- Day 5/day_five.py
+++ Day 5/day_five.py
@@ -5,26 +5,38 @@ from crane_mover import CraneMover9000, CraneMover9001
 from day_five_input import CRATES, INSTRUCTIONS
 
 
-def get_crates():
-    """Return formatted list of crates from input format"""
-    crates = [[] for _ in range(9)]
-    crate_stack = CRATES.splitlines()
-    for line in crate_stack:
-        for index, character in enumerate(line):
-            if character in ascii_uppercase:
-                crates[(index - 1) // 4].append(character)
+class InputProcessor:
+    """Class to Process input from file"""
 
-    return [stack[::-1] for stack in crates]
+    @classmethod
+    def get_crates(cls, raw_input: str):
+        """Return formatted list of crates from input format"""
+        crates = cls.__initialise_crate_stack(cls.__number_of_crate_stacks(raw_input))
+        crate_stack = raw_input.splitlines()[:-1]
+        for line in crate_stack:
+            for index, character in enumerate(line):
+                if character in ascii_uppercase:
+                    crates[(index - 1) // 4].append(character)
+
+        return [stack[::-1] for stack in crates]
+
+    @staticmethod
+    def __initialise_crate_stack(number_of_stacks: int) -> list:
+        return [[] for _ in range(number_of_stacks)]
+
+    @staticmethod
+    def __number_of_crate_stacks(raw_input: str) -> int:
+        return 9
 
 
 def main():  # pylint:disable=missing-function-docstring
     instructions = INSTRUCTIONS.splitlines()
-    elf_crane = CraneMover9000(get_crates())
+    elf_crane = CraneMover9000(InputProcessor.get_crates(CRATES))
     for instruction in instructions:
         elf_crane.move_crates(instruction)
     print(elf_crane.get_accesible_crates())
 
-    elf_crane = CraneMover9001(get_crates())
+    elf_crane = CraneMover9001(InputProcessor.get_crates(CRATES))
     for instruction in instructions:
         elf_crane.move_crates(instruction)
     print(elf_crane.get_accesible_crates())
