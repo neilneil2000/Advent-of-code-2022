84aff449bde88453e16013259b41a19c0abc763d
Neil Jeffery <neil.jeffery@bt.com>
2022-12-06 12:51:25 +0000

    Attempt 4

--- Day 6/day_six.py
+++ Day 6/day_six.py
@@ -25,6 +25,17 @@ def find_start_3(buffer: str, length: int):
     return end
 
 
+def is_unique(buffer: str, end: int, length: int) -> bool:
+    return len(set(buffer[end - length : end])) == length
+
+
+def find_start_4(buffer: str, length: int):
+    for end in range(length, len(buffer)):
+        if is_unique(buffer, end, length):
+            break
+    return end
+
+
 print(find_start(PUZZLE_INPUT))
 print(find_start_3(PUZZLE_INPUT, 4))
 print(find_start_3(PUZZLE_INPUT, 14))
