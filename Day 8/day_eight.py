a76a29926bfc01487f113758532243926772cccd
Neil Jeffery <neil.jeffery@bt.com>
2022-12-08 10:47:20 +0000

    Part 2 Complete

--- Day 8/day_eight.py
+++ Day 8/day_eight.py
@@ -44,11 +44,42 @@ class Grove:
             highest_tree = max(highest_tree, tree_height)
         return False
 
+    def get_highest_scenic_score(self) -> int:
+        """Get Highest Scenic Score in grove"""
+        scores = []
+        for row_id, row in enumerate(self.trees):
+            for column_id, _ in enumerate(row):
+                scores.append(self.get_scenic_score(row_id, column_id))
+
+        return max(scores)
+
+    def get_scenic_score(self, row_id: int, column_id: int) -> int:
+        """Calculate Scenic Score for a given tree"""
+        tree_lines = self.__get_tree_lines(row_id, column_id)
+        scenic_score = 1
+        for tree_line in tree_lines:
+            scenic_score *= self.get_number_of_trees_visible(*tree_line)
+        return scenic_score
+
+    def get_number_of_trees_visible(self, tree_line: list, tree_index: int) -> int:
+        """Returns number of trees to right of tree_index until one of same height is met"""
+        treehouse_height = tree_line[tree_index]
+        tree_line = tree_line[tree_index + 1 :]
+        trees_visible = 0
+        if not tree_line:
+            return trees_visible
+        for tree_height in tree_line:
+            trees_visible += 1
+            if tree_height >= treehouse_height:
+                break
+        return trees_visible
+
 
 def main():
     trees = [list(map(int, list(row))) for row in PUZZLE_INPUT.splitlines()]
     my_grove = Grove(trees)
     print(my_grove.count_visible_trees())
+    print(my_grove.get_highest_scenic_score())
 
 
 if __name__ == "__main__":
