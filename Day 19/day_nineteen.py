820fc8a8edd3ba292aa28d73b10e244824a40b6e
Neil Jeffery <neil.jeffery@bt.com>
2023-01-11 11:31:09 +0000

    Removed Old stuff from Day 19

--- Day 19/day_nineteen.py
+++ Day 19/day_nineteen.py
@@ -1,184 +1,202 @@
 from time import perf_counter
+
 from day_nineteen_input import EXAMPLE_INPUT, PUZZLE_INPUT
 
 
-class Game:
+class DoubleTree:
     def __init__(self, index: int, blueprint: dict, game_time: int = 24) -> None:
         self.game_time = game_time
         self.index = index
         self.costs = blueprint
-        self.maximum_ore = max(self.costs[x]["ore"] for x in self.costs)
-        self.maximum_clay = self.costs["obsidian"]["clay"]
-        self.maximum_obsidian = self.costs["geode"]["obsidian"]
-        self.maximum_rates = {
-            "ore": self.maximum_ore,
-            "clay": self.maximum_clay,
-            "obsidian": self.maximum_obsidian,
-            "geode": 1000,
+        self.maxima = {
+            "ore": max(self.costs[x]["ore"] for x in self.costs),
+            "clay": self.costs["obsidian"]["clay"],
+            "obsidian": self.costs["geode"]["obsidian"],
         }
+        self.paths = {0: {0: {0: {0: {0: {1: {0: 0}}}}}}}
         print(f"Game {self.index} created:")
+
         print(f"Blueprint: {self.costs}")
         print(
-            f"Maximums: Ore-{self.maximum_ore}, Clay-{self.maximum_clay}, Obsidian-{self.maximum_obsidian}"
+            f"Maximums: Ore-{self.maxima['ore']}, Clay-{self.maxima['clay']}, Obsidian-{self.maxima['obsidian']}"
         )
 
-    def save_up(self, resources: dict, robots: dict):
-        return resources, robots
-
-    def collect(self, resources: dict, robots: dict):
-        """Spend time collecting resources"""
-        for resource in resources:
-            resources[resource] += robots[resource]
-
-    def build_ore_robot(self, resources: dict, robots: dict):
-        return self.build_robot(resources, robots, "ore")
-
-    def build_clay_robot(self, resources: dict, robots: dict):
-        return self.build_robot(resources, robots, "clay")
-
-    def build_obsidian_robot(self, resources: dict, robots: dict):
-        return self.build_robot(resources, robots, "obsidian")
-
-    def build_geode_robot(self, resources: dict, robots: dict):
-        return self.build_robot(resources, robots, "geode")
-
-    def build_robot(self, resources: dict, robots: dict, robot_type: str):
-        """Increase number of robots"""
-        for resource in self.costs[robot_type]:
-            resources[resource] -= self.costs[robot_type][resource]
-        robots[robot_type] += 1
-        return resources, robots
-
-    def unrestricted_robots_of_type(self, robots: dict, robot_type: str) -> bool:
-        """Returns True if I can now make an unrestricted amount of robots of robot_type"""
-        for resource in self.costs[robot_type]:
-            if robots[resource] < self.costs[robot_type][resource]:
-                return False
-        return True
-
-    def is_robot_affordable(self, resources: dict, robot_type: str) -> bool:
-        """Returns true if given robot type is affordable"""
-        for resource in self.costs[robot_type]:
-            if resources[resource] < self.costs[robot_type][resource]:
-                return False
-        return True
-
-    def is_robot_affordable_ore(
-        self, resources: dict, robots: dict, robot_type: str
-    ) -> bool:
-        """Returns true if given robot type is affordable given the amount of ore"""
-        if (
-            resources["ore"] < self.costs[robot_type]["ore"]
-            and robots["ore"] < self.costs[robot_type]["ore"]
-        ):
-            return False
-        return True
-
-    def robot_at_max_rate(self, robots: dict, robot_type: str) -> bool:
-        """Returns true if there is no point creating more robots of the given type"""
-        return robots[robot_type] >= self.maximum_rates[robot_type]
-
-    def get_options_optimal(self, resources: dict, robots: dict) -> set:
-        """Return optimal set of options given resources and robots available"""
-        if self.unrestricted_robots_of_type(robots, "geode"):
-            if self.is_robot_affordable(resources, "geode"):
-                return {self.build_geode_robot}
-            return {self.save_up}
-
-        potentials = {"geode", "obsidian", "clay", "ore"}
-        for resource in potentials.copy():
-            if self.robot_at_max_rate(robots, resource):
-                potentials.discard(resource)
-
+    def build_option_combos(self, starting_combo):
+        """Return list of possible combos"""
         options = []
-        for resource in potentials.copy():
-            if not self.is_robot_affordable_ore(resources, robots, resource):
-                options.append(self.save_up)
-            if not self.is_robot_affordable(resources, resource):
-                potentials.discard(resource)
-
-        if "ore" in potentials:
-            options.insert(0, self.build_ore_robot)
-        if "clay" in potentials:
-            options.insert(0, self.build_clay_robot)
-        if "obsidian" in potentials:
-            options.insert(0, self.build_obsidian_robot)
-        if "geode" in potentials:
-            options.insert(0, self.build_geode_robot)
-
-        return options
-
-    def get_options(self, resources: dict, robots: dict) -> set:
-        """Return set of options given resources available"""
-        options = set()
-        if (
-            resources["ore"] >= self.costs["geode"]["ore"]
-            and resources["obsidian"] >= self.costs["geode"]["obsidian"]
-        ):
-            options.add(self.build_geode_robot)
-        if (
-            robots["obsidian"] < self.maximum_obsidian
-            and resources["ore"] >= self.costs["obsidian"]["ore"]
-            and resources["clay"] >= self.costs["obsidian"]["clay"]
-        ):
-            options.add(self.build_obsidian_robot)
-            if (
-                resources["ore"] - self.costs["obsidian"]["ore"] + robots["ore"]
-                < self.costs["geode"]["ore"]
-            ):
-                options.add(self.save_up)
-        if (
-            robots["clay"] < self.maximum_clay
-            and resources["ore"] >= self.costs["clay"]["ore"]
-        ):
-            options.add(self.build_clay_robot)
-            if (
-                resources["ore"] - self.costs["clay"]["ore"] + robots["ore"]
-                < self.costs["geode"]["ore"]
-                or resources["ore"] - self.costs["clay"]["ore"] + robots["ore"]
-                < self.costs["obsidian"]["ore"]
-            ):
-                options.add(self.save_up)
-        if (
-            robots["ore"] < self.maximum_ore
-            and resources["ore"] >= self.costs["ore"]["ore"]
-        ):
-            options.add(self.build_ore_robot)
-
-        if not options:
-            options.add(self.save_up)
-
-        return options
-
-    def best_case(self, time_left: int, robots: dict) -> int:
-        current_robots = robots["geode"] * time_left
-        new_robots = (time_left * (time_left - 1)) // 2
-        return current_robots + new_robots
-
-    def take_turn(
-        self, resources: dict, robots: dict, time_left: int, best_so_far: int
-    ):
-        """Execute a single 1 minute round of the game"""
-        if time_left == 1:
-            return resources["geode"] + robots["geode"]
-        if best_so_far >= resources["geode"] + self.best_case(time_left, robots):
-            return -1  # no-hoper
-        options = self.get_options_optimal(resources, robots)
-        self.collect(resources, robots)
-        for option in options:
-            new_resources, new_robots = option(resources.copy(), robots.copy())
-            best_so_far = max(
-                best_so_far,
-                self.take_turn(new_resources, new_robots, time_left - 1, best_so_far),
+        g, g_r, ob, ob_r, o, o_r, c, c_r = starting_combo
+        # Build Geode Robot
+        if o >= self.costs["geode"]["ore"] and ob >= self.costs["geode"]["obsidian"]:
+            options.append(
+                [
+                    g + g_r,
+                    g_r + 1,
+                    ob + ob_r - self.costs["geode"]["obsidian"],
+                    ob_r,
+                    o + o_r - self.costs["geode"]["ore"],
+                    o_r,
+                    c + c_r,
+                    c_r,
+                ]
             )
-        return best_so_far
+        # Build Obsidian Robot
+        if (
+            o >= self.costs["obsidian"]["ore"]
+            and c >= self.costs["obsidian"]["clay"]
+            and ob_r < self.maxima["obsidian"]
+        ):
+            options.append(
+                [
+                    g + g_r,
+                    g_r,
+                    ob + ob_r,
+                    ob_r + 1,
+                    o + o_r - self.costs["obsidian"]["ore"],
+                    o_r,
+                    c + c_r - self.costs["obsidian"]["clay"],
+                    c_r,
+                ]
+            )
+        # Build Clay Robot
+        if o >= self.costs["clay"]["ore"] and c_r < self.maxima["clay"]:
+            options.append(
+                [
+                    g + g_r,
+                    g_r,
+                    ob + ob_r,
+                    ob_r,
+                    o + o_r - self.costs["clay"]["ore"],
+                    o_r,
+                    c + c_r,
+                    c_r + 1,
+                ]
+            )
+        # Build Ore Robot
+        if o >= self.costs["ore"]["ore"] and o_r < self.maxima["ore"]:
+            options.append(
+                [
+                    g + g_r,
+                    g_r,
+                    ob + ob_r,
+                    ob_r,
+                    o + o_r - self.costs["ore"]["ore"],
+                    o_r + 1,
+                    c + c_r,
+                    c_r,
+                ]
+            )
+        # Save
+        options.append(
+            [
+                g + g_r,
+                g_r,
+                ob + ob_r,
+                ob_r,
+                o + o_r,
+                o_r,
+                c + c_r,
+                c_r,
+            ]
+        )
+        return options
+
+    def add_combo_to_tree(self, combo, tree):
+        a, b, c, d, e, f, g, h = combo
+        try:
+            tree[a][b][c][d][e][f][g] = max(h, tree[a][b][c][d][e][f][g])
+        except KeyError:
+            if tree.get(a) is None:
+                tree[a] = {b: {c: {d: {e: {f: {g: h}}}}}}
+            elif tree[a].get(b) is None:
+                tree[a][b] = {c: {d: {e: {f: {g: h}}}}}
+            elif tree[a][b].get(c) is None:
+                tree[a][b][c] = {d: {e: {f: {g: h}}}}
+            elif tree[a][b][c].get(d) is None:
+                tree[a][b][c][d] = {e: {f: {g: h}}}
+            elif tree[a][b][c][d].get(e) is None:
+                tree[a][b][c][d][e] = {f: {g: h}}
+            elif tree[a][b][c][d][e].get(f) is None:
+                tree[a][b][c][d][e][f] = {g: h}
+            elif tree[a][b][c][d][e][f].get(g) is None:
+                tree[a][b][c][d][e][f][g] = h
+
+    def play_round(self):
+        new_paths = {}
+        for g, a in self.paths.items():
+            for g_r, b in a.items():
+                for ob, c in b.items():
+                    for ob_r, d in c.items():
+                        for o, e in d.items():
+                            for o_r, f in e.items():
+                                for c, c_r in f.items():
+                                    combo = [g, g_r, ob, ob_r, o, o_r, c, c_r]
+                                    for option in self.build_option_combos(combo):
+                                        self.add_combo_to_tree(option, new_paths)
+
+        self.paths = new_paths
+
+    def print_combos(self, summary_only: bool = False) -> None:
+        total = 0
+        if not summary_only:
+            print(
+                "============================================================================"
+            )
+            print(
+                "geodes|geode_robots|obsidian|obsidian_robots|ore|ore_robots|clay|clay_robots"
+            )
+            print(
+                "----------------------------------------------------------------------------"
+            )
+        for g, a in self.paths.items():
+            for g_r, b in a.items():
+                for ob, c in b.items():
+                    for ob_r, d in c.items():
+                        for o, e in d.items():
+                            for o_r, f in e.items():
+                                for c, c_r in f.items():
+                                    if not summary_only:
+                                        print(
+                                            f"{g}     | {g_r}          | {ob}      | {ob_r}             | {o} | {o_r}        | {c}  | {c_r}"
+                                        )
+                                    total += 1
+        if not summary_only:
+            print(
+                "============================================================================"
+            )
+        print(f"{total} combos")
+
+    def get_combos(self) -> None:
+        total = 0
+        for g, a in self.paths.items():
+            for g_r, b in a.items():
+                for ob, c in b.items():
+                    for ob_r, d in c.items():
+                        for o, e in d.items():
+                            for o_r, f in e.items():
+                                for c, c_r in f.items():
+                                    total += 1
+        return total
 
     def run_game(self) -> int:
         """Return Maximum number of geodes that can be harvested in a game"""
-        initial_resources = {"ore": 0, "clay": 0, "obsidian": 0, "geode": 0}
-        initial_robots = {"ore": 1, "clay": 0, "obsidian": 0, "geode": 0}
-        initial_time = self.game_time
-        return self.take_turn(initial_resources, initial_robots, initial_time, 0)
+        for i in range(self.game_time - 1):
+            start = perf_counter()
+            self.play_round()
+            end = perf_counter()
+            combos = self.get_combos()
+            print(
+                f"Round {i+1} Complete in {end-start} seconds with {combos} combos",
+                end="\r",
+            )
+            pass
+
+        return self.get_max_geodes()
+
+    def get_max_geodes(self):
+        best = 0
+        for geodes, robots in self.paths.items():
+            best = max(best, geodes + max(robots.keys()))
+        return best
 
     def get_quality_score(self):
         """Return Quality Score as defined in problem statement"""
@@ -211,19 +229,21 @@ def parse_input(puzzle_input: str):
 
 def main():  # pylint:disable=missing-function-docstring
     blueprints = parse_input(PUZZLE_INPUT)
-    total = 0
-    for index, blueprint in blueprints.items():
-        start = perf_counter()
-        #total += Game(index, blueprint, 24).get_quality_score()
-        end = perf_counter()
-        print(f"Blueprint {index} assessed in {end-start}")
-    print(total)
+    total = 1
 
     for index, blueprint in blueprints.items():
         if index == 4:
             break
         start = perf_counter()
-        total *= Game(index, blueprint, 32).run_game()
+        geodes = DoubleTree(index, blueprint, 32).run_game()
+        end = perf_counter()
+        total *= geodes
+        print(f"Blueprint {index} assessed in {end-start} Total Geodes={geodes}")
+    print(total)
+
+    for index, blueprint in blueprints.items():
+        start = perf_counter()
+        total += DoubleTree(index, blueprint, 24).get_quality_score()
         end = perf_counter()
         print(f"Blueprint {index} assessed in {end-start}")
     print(total)
